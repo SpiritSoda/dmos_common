e0635d6bd71e992fc7bc29dc90f78d56ce297b5f
SpiritSoda <1800013119@pku.edu.cn>
2022-11-22 10:21:33 +0800

    [Adjust] update data type in DMOSResponse

--- data/DMOSResponse.java
+++ data/DMOSResponse.java
@@ -4,15 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 @Data
 public class DMOSResponse {
     private int code;
-    private HashMap<String, String> data;
-    public DMOSResponse(int code, HashMap<String, String> data){
+    private HashMap<String, Object> data;
+    public DMOSResponse(int code, HashMap<String, Object> data){
         this.code = code;
         this.data = data;
     }
-    public static DMOSResponse buildSuccessResponse(HashMap<String, String> data) { return new DMOSResponse(0, data); }
-    public static DMOSResponse buildFailsResponse(HashMap<String, String> data) { return new DMOSResponse(-1, data); }
+    public static DMOSResponse buildSuccessResponse(HashMap<String, Object> data) { return new DMOSResponse(0, data); }
+    public static DMOSResponse buildFailsResponse(HashMap<String, Object> data) { return new DMOSResponse(-1, data); }
 }
