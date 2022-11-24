7a6be1e69869284f026b874ce40aa370df85b68f
SpiritSoda <1800013119@pku.edu.cn>
2022-11-24 16:15:48 +0800

    [Updated] add channel util

---
+++ util/ChannelUtil.java
@@ -0,0 +1,16 @@
+package com.dmos.dmos_common.util;
+
+import com.dmos.dmos_common.message.Message;
+import com.dmos.dmos_common.message.MessageType;
+import io.netty.channel.Channel;
+
+
+public class ChannelUtil {
+    public static void heartbeat(Channel channel){
+        if(channel == null || channel.isActive())
+            return;
+        Message message = new Message();
+        message.setType(MessageType.HEARTBEAT);
+        channel.writeAndFlush(ParseUtil.encode(message, false));
+    }
+}
